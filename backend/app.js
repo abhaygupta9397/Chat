28d870f8809265046a189dbb6772c9f28dbf21f8
Abhay Gupta <abhaygupta9397@gmail.com>
2025-06-16 22:51:21 +0530

    User Model done

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connect from './db/db.js';
+import userRoutes from './routes/user.route.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';    
 
 
 connect();
@@ -11,9 +14,14 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));                // log requests to the console
 app.use(express.json());              // convert json data to object -- interesting
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/users', userRoutes);
+
 
 app.get('/',(req,res)=>{
     res.send('hello mic check');
